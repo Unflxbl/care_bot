0cf1aafc50ba9c1ac3ce003c6793c90dd7fc682b
mpwow5 <mpwow5@gmail.com>
2019-08-15 01:23:46 +0300

    первая версия

---
+++ care_bot.py
@@ -0,0 +1,13 @@
+import os
+import time
+import re
+import slack
+
+from slack import WebClient
+
+client = slack.WebClient(token="xoxb-714578520370-719690847425-oB0mKbJGvWejGlGeyLs0uvR9")
+
+client.chat_postMessage(
+  channel="general",
+  text="Настюшка = котка :heart:"
+)
