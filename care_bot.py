8034a19d208e341736fb7f58363b26d070a56d11
mpwow5 <mpwow5@gmail.com>
2019-08-19 21:47:25 +0300

    666

--- care_bot.py
+++ care_bot.py
@@ -1,65 +1,35 @@
 import os
+import os
 import time
 import re
 import slack
 
 from slack import WebClient
+import flask
+from flask import Flask, request, Response
 
-client = slack.WebClient(token="xoxb-714578520370-719690847425-rpvmLyrfh2tn2yuu8Z5WcRbO")
+client = slack.WebClient(token="xoxb-714578520370-719690847425-7xvdRLMedjGdeLKQ152t41Gt")
 
-client.chat_postMessage(
-  channel="general",
-  blocks=[
-    {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": "*Контекстная реклама: основы*"
-      }
-    },
-    {
-      "type": "divider"
-    },
-    {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": "Знакомство с Яндекс.Директом\n <https://help.elama.global/hc/ru/articles/207432709|Прочитать>"
-      },
-      "accessory": {
-        "type": "image",
-        "image_url": "https://www.meme-arsenal.com/memes/148db78d7a53e8fe680408a6ad60c2c4.jpg",
-        "alt_text": "лама"
-      }
-    },
-    {
-      "type": "divider"
-    },
-    {
-      "type": "section",
-      "fields": [
-        {
-          "type": "mrkdwn",
-          "text": "Справился?"
-        }
-      ]
-    },
-    {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": "."
-      },
-      "accessory": {
-        "type": "button",
-        "text": {
-          "type": "plain_text",
-          "text": "Click Me"
-        },
-        "value": "click_me_123",
-        "action_id": "button"
-      }
-    }
-  ]
-)
+app = Flask(__name__)
 
+def send_message(channel_id, message):
+    client.chat_postMessage(
+      channel=channel_id,
+      text=message,
+    )
+
+@app.route('/test', methods=['POST'])
+def test():
+
+    my_var = flask.request.values
+    print(my_var)
+
+    send_message(channel_id="CM0GYQC7M", message="Hello!")
+    return Response(), 200
+
+@app.route('/', methods=['GET'])
+def yes():
+    return Response('It works!')
+
+if __name__ == "__main__":
+    app.run(debug=True)
